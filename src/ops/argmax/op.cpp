e697667d77d6271cb7a503b16d1892773c0b3108
luoyueyuguang <luoyuelight@outlook.com>
2026-01-26 15:58:38 +0800

    add embedding

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -6,6 +6,16 @@ void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
     // TO_BE_IMPLEMENTED();
     CHECK_SAME_DEVICE(max_idx, max_val, vals);
     CHECK_SAME_DTYPE(max_val -> dtype(), vals -> dtype());
+    if (max_idx -> deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::argmax(
+            vals -> data(),
+            max_val -> data(),
+            reinterpret_cast<int64_t *>(max_idx -> data()),
+            vals -> dtype(),
+            vals -> numel()
+        );
+    }
+    llaisys::core::context().setDevice(vals -> deviceType(), vals -> deviceId());
     switch (vals -> deviceType()) {
         case LLAISYS_DEVICE_CPU:
             llaisys::ops::cpu::argmax(
